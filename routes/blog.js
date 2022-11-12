ff870108f689cdf5a40e914ac817e8650f171fe0
m-tonon <math.tonon@gmail.com>
2022-11-12 06:38:22 -0300

    inserting dynamic data & fetching & displaying a list of blog posts

--- routes/blog.js
+++ routes/blog.js
@@ -8,8 +8,13 @@ router.get('/', function (req, res) {
   res.redirect('/posts');
 });
 
-router.get('/posts', function (req, res) {
-  res.render('posts-list');
+router.get('/posts', async function (req, res) {
+  const query = `
+  SELECT posts.*, authors.name AS author_name from posts 
+  INNER JOIN authors ON posts.author_id = authors.id
+  `; // for more readability
+  const [posts] = await db.query(query);
+  res.render('posts-list', { posts: posts });
 });
 
 router.get('/new-post', async function (req, res) {
@@ -19,6 +24,22 @@ router.get('/new-post', async function (req, res) {
   // the first element is the records, and the second holds metadata
   // [authors] ==> desconstruction the array spreeding out the values and stores in authors
   res.render('create-post', { authors: authors });
+});
+
+router.post('/posts', async function (req, res) {
+  // listen to post request
+  const data = [
+    req.body.title, // hold the incomming data (name form tag from create post)
+    req.body.summary,
+    req.body.content,
+    req.body.author,
+  ];
+  await db.query('INSERT INTO posts (title, summary, body, author_id) VALUES (?)',[
+    data
+  ]);
+  // '?' place holder that will be replaced by the array '[data]'
+  // the package mysql will automatically spread out into separated values
+  res.redirect('/posts');
 });
 
 module.exports = router;
